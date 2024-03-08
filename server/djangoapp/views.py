3dac498a62d9c0aab02f7503d333613d3281b7d2
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-08 16:51:27 -0500

    Add logout routes and functions

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -38,9 +38,12 @@ def login_user(request):
         data = {"userName": username, "status": "Authenticated"}
     return JsonResponse(data)
 
+
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    data = {"userName":""}
+    return JsonResponse(data)
+
 
 # Create a `registration` view to handle sign up request
 # @csrf_exempt
