beb73cc2e51314389b2abcb8ece7b736ce7bae41
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-09 16:45:07 -0500

    Add specific dealer view to frontend

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,4 +1,5 @@
 import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
     </Routes>
