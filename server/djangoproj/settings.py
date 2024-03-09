3850221924397a54052d0535abe6e8dc30903217
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-09 12:39:45 -0500

    Add models registrations for django admin

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS=[
 ]
 CSRF_TRUSTED_ORIGINS=[
     'https://j8ahmed-8000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai',
+    # Not mentioned but may be needed
     'https://j8ahmed-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
 ]
 
