4681b39711ea403bdad5c2505d716972925087ad
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-09 16:36:37 -0500

    Add dealers view to frontend

--- server/djangoproj/urls.py
+++ server/djangoproj/urls.py
@@ -22,6 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('about/', TemplateView.as_view(template_name="About.html")),
     path('contact/', TemplateView.as_view(template_name="Contact.html")),
+    path('dealers/', TemplateView.as_view(template_name="index.html")),
     path('login/', TemplateView.as_view(template_name="index.html")),
     path('register/', TemplateView.as_view(template_name="index.html")),
     path('admin/', admin.site.urls),
--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+import Dealers from './components/Dealers/Dealers';
 import LoginPanel from "./components/Login/Login"
 import RegisterPanel from "./components/Register/Register"
 import { Routes, Route } from "react-router-dom";
@@ -5,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <Routes>
+      <Route path="/dealers" element={<Dealers/>} />
       <Route path="/login" element={<LoginPanel />} />
       <Route path="/register" element={<RegisterPanel />} />
     </Routes>
